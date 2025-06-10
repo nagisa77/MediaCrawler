74d86ab5ab300a49e1430c5fd2ceac51e7690d83
tim <nagisa12164@163.com>
2025-06-10 11:13:12 +0800

    feat: db-config

--- config/db_config.py
+++ config/db_config.py
@@ -12,7 +12,7 @@
 import os
 
 # mysql config
-RELATION_DB_PWD = os.getenv("RELATION_DB_PWD", "123456")
+RELATION_DB_PWD = os.getenv("RELATION_DB_PWD", "qq1216414009")
 RELATION_DB_USER = os.getenv("RELATION_DB_USER", "root")
 RELATION_DB_HOST = os.getenv("RELATION_DB_HOST", "localhost")
 RELATION_DB_PORT = os.getenv("RELATION_DB_PORT", 3306)
@@ -21,7 +21,7 @@ RELATION_DB_NAME = os.getenv("RELATION_DB_NAME", "media_crawler")
 
 # redis config
 REDIS_DB_HOST = "127.0.0.1"  # your redis host
-REDIS_DB_PWD = os.getenv("REDIS_DB_PWD", "123456")  # your redis password
+REDIS_DB_PWD = os.getenv("REDIS_DB_PWD", "")  # your redis password
 REDIS_DB_PORT = os.getenv("REDIS_DB_PORT", 6379)  # your redis port
 REDIS_DB_NUM = os.getenv("REDIS_DB_NUM", 0)  # your redis db num
 
