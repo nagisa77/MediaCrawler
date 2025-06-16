e5633ac35c2d70101b4be8fa166a675ff6ab1d2d
tim <nagisa12164@163.com>
2025-06-16 12:58:05 +0800

    feat: prompt 调整

--- config/db_config.py
+++ config/db_config.py
@@ -12,9 +12,9 @@
 import os
 
 # mysql config
-RELATION_DB_PWD = os.getenv("RELATION_DB_PWD", "qq1216414009")
-RELATION_DB_USER = os.getenv("RELATION_DB_USER", "root")
-RELATION_DB_HOST = os.getenv("RELATION_DB_HOST", "localhost")
+RELATION_DB_PWD = os.getenv("RELATION_DB_PWD")
+RELATION_DB_USER = os.getenv("RELATION_DB_USER")
+RELATION_DB_HOST = os.getenv("RELATION_DB_HOST")
 RELATION_DB_PORT = os.getenv("RELATION_DB_PORT", 3306)
 RELATION_DB_NAME = os.getenv("RELATION_DB_NAME", "media_crawler")
 
